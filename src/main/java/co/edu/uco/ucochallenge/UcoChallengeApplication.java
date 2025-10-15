f42620684abed338f1c5a313fdd2b9030fcf0a20
juancabernal <juancamilobernal2222@gmail.com>
2025-10-15 15:10:22 -0500

    Cambio de Prueba Commit de prueba para eclipse

--- src/main/java/co/edu/uco/ucochallenge/UcoChallengeApplication.java
+++ src/main/java/co/edu/uco/ucochallenge/UcoChallengeApplication.java
@@ -3,6 +3,7 @@ package co.edu.uco.ucochallenge;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+// Main application class for the UcoChallenge Spring Boot application.
 @SpringBootApplication
 public class UcoChallengeApplication {
 
