6c9291bf96d3c47d7cc833ec0b880e9833b7ff39
Andres Velez <andresjobpk@gmail.com>
2025-10-25 01:05:31 -0500

    Prerapando main para oraganizar repo

--- software2-proyecto-clase-eclipse/src/main/java/co/edu/uco/ucochallenge/user/registeruser/application/interactor/usecase/RegisterUserUseCase.java
+++
@@ -1,9 +0,0 @@
-package co.edu.uco.ucochallenge.user.registeruser.application.interactor.usecase;
-
-import co.edu.uco.ucochallenge.application.Void;
-import co.edu.uco.ucochallenge.application.interactor.usecase.UseCase;
-import co.edu.uco.ucochallenge.user.registeruser.application.usecase.domain.RegisterUserDomain;
-
-public interface RegisterUserUseCase extends UseCase<RegisterUserDomain, Void> {
-
-}
