db2c7d9768b5d08c62da2818fc59a6cfec55f5d0
Tomas Gomez Cifuentes <tomicifuentesposada@gmail.com>
2025-10-26 21:56:58 -0500

    Micro-servicios conectados al Eureka

--- message-service/src/main/java/co/edu/uco/messageservice/MessageServiceApplication.java
+++ message-service/src/main/java/co/edu/uco/messageservice/MessageServiceApplication.java
@@ -2,12 +2,12 @@ package co.edu.uco.messageservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class MessageServiceApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(MessageServiceApplication.class, args);
 	}
-
 }
--- parameters-service/src/main/java/co/edu/uco/parametersservice/ParametersServiceApplication.java
+++ parameters-service/src/main/java/co/edu/uco/parametersservice/ParametersServiceApplication.java
@@ -2,12 +2,13 @@ package co.edu.uco.parametersservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class ParametersServiceApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ParametersServiceApplication.class, args);
 	}
-
 }
