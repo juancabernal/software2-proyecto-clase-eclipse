d31b054780a7d96c1bd329cf7f1c0e17e9050ef6
Andres Velez <andres.velez5136@uco.net.co>
2025-10-26 23:22:17 -0500

    Eliminacion de la carpeta SRC random para no usar ni tener en cuenta

--- src/main/java/co/edu/uco/ucochallenge/user/registeruser/application/usecase/rules/Rule.java
+++
@@ -1,8 +0,0 @@
-package co.edu.uco.ucochallenge.user.registeruser.application.usecase.rules;
-
-import co.edu.uco.ucochallenge.user.registeruser.application.usecase.rules.context.ContextRegisterUser;
-
-public interface Rule {
-    String getName();
-    boolean evaluate(ContextRegisterUser context);
-}
