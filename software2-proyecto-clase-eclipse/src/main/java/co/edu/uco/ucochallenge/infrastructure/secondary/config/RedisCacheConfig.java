9cc99c58f616e0be755259a09ef7bf432467bb0e
ZunoSuninsqui <juanpisave27@gmail.com>
2025-11-04 02:17:00 -0500

    Incorporando el cache en usuarios, actualmente solo está incorporado para la consulta de un único usuario

---
+++ software2-proyecto-clase-eclipse/src/main/java/co/edu/uco/ucochallenge/infrastructure/secondary/config/RedisCacheConfig.java
@@ -0,0 +1,29 @@
+package co.edu.uco.ucochallenge.infrastructure.secondary.config;
+
+import co.edu.uco.ucochallenge.application.user.getUser.dto.GetUserOutputDTO;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.cache.RedisCacheConfiguration;
+import org.springframework.data.redis.cache.RedisCacheManager;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.serializer.Jackson2JsonRedisSerializer;
+import org.springframework.data.redis.serializer.RedisSerializationContext;
+
+import java.time.Duration;
+
+@Configuration
+public class RedisCacheConfig {
+
+    @Bean
+    public RedisCacheManager cacheManager(RedisConnectionFactory redisConnectionFactory) {
+        RedisCacheConfiguration cacheConfiguration = RedisCacheConfiguration.defaultCacheConfig()
+                .entryTtl(Duration.ofMinutes(10))
+                .disableCachingNullValues()
+                .serializeValuesWith(RedisSerializationContext.SerializationPair.fromSerializer(
+                        new Jackson2JsonRedisSerializer<>(GetUserOutputDTO.class)));
+
+        return RedisCacheManager.builder(redisConnectionFactory)
+                .cacheDefaults(cacheConfiguration)
+                .build();
+    }
+}
