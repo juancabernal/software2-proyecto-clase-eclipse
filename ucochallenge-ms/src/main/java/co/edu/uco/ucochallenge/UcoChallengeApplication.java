a63cbb34b9144f0ed773898ffe3e7e7d31f84652
juancabernal <juancamilobernal2222@gmail.com>
2025-11-06 20:07:11 -0500

    BACKUP

--- ucochallenge-ms/src/main/java/co/edu/uco/ucochallenge/UcoChallengeApplication.java
+++ ucochallenge-ms/src/main/java/co/edu/uco/ucochallenge/UcoChallengeApplication.java
@@ -3,6 +3,8 @@ package co.edu.uco.ucochallenge;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+
+//.
 @SpringBootApplication
 public class UcoChallengeApplication {
 
